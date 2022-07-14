aac86f802fe637ca83d0f2dd3efb9020ddcb7390
budayyy <budibuday97@gmail.com>
2022-07-14 20:06:13 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2E6A50',
+        secondary: '#64748b',
+        dark: '#1e293b',
+      },
+    },
+  },
+  plugins: [],
+}
